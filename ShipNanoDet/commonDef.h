67932995216f69b32bdaf93b2b2b8265bed1bff7
riskycheng <riskycheng@gmail.com>
2022-04-24 11:49:58 +0800

    add and expose more fields in app_config.json

--- ShipNanoDet/commonDef.h
+++ ShipNanoDet/commonDef.h
@@ -9,7 +9,14 @@ struct ShipBox {
 	float conf = 0.f;
 };
 struct FrameResult {
+	/* cameraID used for identify where the stream comes from, need to set it inside the json config */
+	int cameraID;
 	int frameID;
 	std::vector<ShipBox> boxes;
 	bool isInDanger;
+	/* expose the dangerous region indicating the configs from json file */
+	int* dangerousRegion = new int[8];
+
+	/* the timestamp for this current frame */
+	string timeStamp;
 };
